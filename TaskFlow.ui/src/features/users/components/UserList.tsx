4b480a31354640cfcde16427e8671a9ab6779365
saqibamin257 <saqibamin257@gmail.com>
2026-04-20 15:58:29 +0500

    Added ReactQuery for api call

--- TaskFlow.ui/src/features/users/components/UserList.tsx
+++ TaskFlow.ui/src/features/users/components/UserList.tsx
@@ -1,21 +1,49 @@
 import { useUsers } from "../hooks/useUsers";
-
-
 export const UserList = () => {
-  const { users, loading, error } = useUsers();
+  const { data: users, isLoading, error, isFetching } = useUsers();
 
-  if (loading) return <p>Loading users...</p>;
-  if (error) return <p style={{ color: "red" }}>{error}</p>;
+  if (isLoading) return <p>Loading users...</p>;
 
-  return (
-    <div>
-      {users.map((u) => (
-        <div key={u.id}>
-          <p>
-            {u.name} -- {u.email}
-          </p>
-        </div>
-      ))}
-    </div>
-  );
+  if (error) return <p style={{ color: "red" }}>Failed to load users</p>;
+
+  return(
+        <div>
+         <h2>Users</h2>
+         {/* 🔄 Background refetch indicator */}
+         {isFetching && <p style={{ fontSize: "12px" }}>Refreshing...</p>}
+         {/* 📭 Empty state */}
+         {users?.length === 0
+         ?
+         <p>No users found</p>
+         :
+         users?.map((u)=>(
+         <div key={u.id}>
+            <p>
+             {u.name} -- {u.email}
+           </p>
+         </div>
+         ))        
+        }
+        </div>);
 };
+
+// import { useUsers } from "../hooks/useUsers";
+
+// export const UserList = () => {
+//   const { users, loading, error } = useUsers();
+
+//   if (loading) return <p>Loading users...</p>;
+//   if (error) return <p style={{ color: "red" }}>{error}</p>;
+
+//   return (
+//     <div>
+//       {users.map((u) => (
+//         <div key={u.id}>
+//           <p>
+//             {u.name} -- {u.email}
+//           </p>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
