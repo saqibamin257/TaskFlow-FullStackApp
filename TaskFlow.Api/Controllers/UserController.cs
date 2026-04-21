7836077735b808100d3060b18c977aaac7d4a925
saqibamin257 <saqibamin257@gmail.com>
2026-04-21 15:21:19 +0500

    added CreateUser Endpoint

--- TaskFlow.Api/Controllers/UserController.cs
+++ TaskFlow.Api/Controllers/UserController.cs
@@ -7,17 +7,21 @@ namespace TaskFlow.Api.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<List<UserDTO>> Get() 
-        {
-            try 
-            {
-                List<UserDTO> users = new List<UserDTO> 
+
+        private static List<UserDTO> userList = new List<UserDTO>
                 {
                   new UserDTO {Id=1,Name="Saqib Amin",Email="sa@gmail.com"},
                   new UserDTO {Id=2,Name="Waqar Amin",Email="wa@gmail.com" }
                 };
-                return users;
+
+
+
+        [HttpGet]
+        public ActionResult<List<UserDTO>> Get() 
+        {
+            try 
+            {                
+                return userList;
             }
             catch (Exception ex) 
             {
@@ -25,9 +29,18 @@ namespace TaskFlow.Api.Controllers
                 return new List<UserDTO>();
             }
         }
+        [HttpPost]
+        public ActionResult Post(UserDTO user)
+        {
+            user.Id = new Random().Next(3, 1000);
+            userList.Add(user);
+            return Ok(user);
+        }
+
     }
 
-   
+
+    
 
 
     public class UserDTO 
