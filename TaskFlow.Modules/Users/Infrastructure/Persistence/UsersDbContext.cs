9d46791f9a0bea2e57171cf32dec1001146517b0
saqibamin257 <saqibamin257@gmail.com>
2026-05-06 18:16:16 +0500

    added EFCore Foundation with Crud functions for User

---
+++ TaskFlow.Modules/Users/Infrastructure/Persistence/UsersDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using TaskFlow.Modules.Users.Domain.Entities;
+
+namespace TaskFlow.Modules.Users.Infrastructure.Persistence
+{
+    public class UsersDbContext:DbContext
+    {
+        public UsersDbContext(DbContextOptions<UsersDbContext> options) : base(options) 
+        {
+        }
+        public DbSet<User> Users => Set<User>();
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(
+                typeof(UsersDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
