1426ea94c078736834ec1e521e9a461e81324443
saqibamin257 <saqibamin257@gmail.com>
2026-05-06 18:16:16 +0500

    added EFCore Foundation with Crud functions for User

---
+++ TaskFlow.Modules/Users/Infrastructure/Configurations/UserConfiguration.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskFlow.Modules.Users.Domain.Entities;
+
+
+namespace TaskFlow.Modules.Users.Infrastructure.Configurations
+{
+    public class UserConfiguration :IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder) 
+        {
+
+
+            // ----- All SQL related rules stay here like lengths, indexes and constraints  ----------
+            // Table
+            // ------------------------------
+            builder.ToTable("Users");
+
+            // ------------------------------
+            // Primary Key
+            // ------------------------------
+            builder.HasKey(x => x.Id);
+
+            // ------------------------------
+            // Properties
+            // ------------------------------
+            builder.Property(x => x.Name)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(x => x.Email)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(x => x.PasswordHash)
+                .IsRequired();
+
+            builder.Property(x => x.Role)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(x => x.TenantId)
+                .IsRequired();
+
+            builder.Property(x => x.IsActive)
+                .IsRequired();
+
+            builder.Property(x => x.CreatedAtUTC)
+                .IsRequired();
+
+            // ------------------------------
+            // Indexes
+            // ------------------------------
+            builder.HasIndex(x => x.Email)
+                .IsUnique();
+        }
+    }
+}
---
+++ TaskFlow.Modules/Users/Infrastructure/Persistence/UsersDbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskFlow.Modules.Users.Infrastructure.Persistence
+{
+    public class UsersDbContextFactory
+    : IDesignTimeDbContextFactory<UsersDbContext>
+    {
+        public UsersDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<UsersDbContext>();
+
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new UsersDbContext(optionsBuilder.Options);
+        }
+    }
+}
