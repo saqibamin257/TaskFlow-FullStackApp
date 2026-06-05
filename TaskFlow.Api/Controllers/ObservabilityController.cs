981712b733c9ed621196d22a9843c6bbc28ee6a6
saqibamin257 <saqibamin257@gmail.com>
2026-06-05 13:24:49 +0500

    setup for OpenTelemetry with Signoz

---
+++ TaskFlow.Api/Controllers/ObservabilityController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskFlow.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ObservabilityController : ControllerBase
+    {
+        [HttpGet("ping")]
+        public IActionResult Ping()
+        {
+            return Ok("Telemetry working");
+        }
+
+        [HttpGet("slow")]
+        public async Task<IActionResult> Slow()
+        {
+            await Task.Delay(5000);
+
+            return Ok("Slow endpoint completed");
+        }
+
+        [HttpGet("error")]
+        public IActionResult Error()
+        {
+            throw new Exception("Test exception");
+        }
+
+        [HttpGet("cpu")]
+        public IActionResult Cpu()
+        {
+            long result = 0;
+
+            for (long i = 0; i < 500000000; i++)
+            {
+                result += i;
+            }
+
+            return Ok(result);
+        }
+    }
+}
---
+++ TaskFlow.Api/Extensions/ObservabilityExtensions.cs
@@ -0,0 +1,54 @@
+﻿using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+
+namespace TaskFlow.Api.Extensions
+{
+    public static class ObservabilityExtensions
+    {
+        public static IServiceCollection AddObservability(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            Console.WriteLine("-$#$$#$#$#------OpenTelemetry Registered-------------$#$$#$#$#------");
+            const string serviceName = "TaskFlow.Api";
+
+            services.AddOpenTelemetry()
+                .ConfigureResource(resource =>
+                {
+                    resource.AddService(serviceName);
+                })
+                .WithTracing(tracing =>
+                {
+                    tracing
+                        .AddAspNetCoreInstrumentation()
+                        .AddHttpClientInstrumentation()
+                        .AddSqlClientInstrumentation(options =>
+                        {
+                            options.RecordException = true;
+                        })
+                        .AddConsoleExporter()
+                        .AddOtlpExporter(options =>
+                        {
+                            options.Endpoint = new Uri("http://localhost:4318");
+                            options.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.HttpProtobuf;
+                        });
+                })
+                .WithMetrics(metrics =>
+                {
+                    metrics
+                        .AddAspNetCoreInstrumentation()
+                        .AddHttpClientInstrumentation()
+                        .AddRuntimeInstrumentation()
+                        .AddOtlpExporter(options =>
+                        {
+                            options.Endpoint = new Uri("http://localhost:4318");
+                            options.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.HttpProtobuf;
+                        });
+                });
+
+            return services;
+        }
+    }
+}
