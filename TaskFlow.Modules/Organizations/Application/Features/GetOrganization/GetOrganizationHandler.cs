25b01482f3005d980e5fe332763fc0baa7d5eaf1
saqibamin257 <saqibamin257@gmail.com>
2026-06-29 12:45:12 +0500

    added GetOrganization Feature

---
+++ TaskFlow.Modules/Organizations/Application/Features/GetOrganization/GetOrganizationHandler.cs
@@ -0,0 +1,47 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskFlow.BuildingBlocks.Localization;
+using TaskFlow.Modules.Organizations.Application.Abstraction;
+
+namespace TaskFlow.Modules.Organizations.Application.Features.GetOrganization
+{
+    public sealed class GetOrganizationHandler : IRequestHandler<GetOrganizationQuery, GetOrganizationResponse>
+    {
+        private readonly IOrganizationRepository _repository;
+
+        public GetOrganizationHandler(
+            IOrganizationRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetOrganizationResponse> Handle(
+            GetOrganizationQuery request,
+            CancellationToken cancellationToken)
+        {
+            var organization =
+                await _repository.GetByIdAsync(
+                    request.Id,
+                    cancellationToken);
+
+            if (organization is null)
+            {
+                throw new KeyNotFoundException(
+                    ErrorKeys.OrganizationNotFound);
+            }
+
+            return new GetOrganizationResponse(
+                organization.Id,
+                organization.Name,
+                organization.Slug,
+                organization.Description,
+                organization.LogoUrl,
+                organization.OwnerUserId,
+                organization.IsActive,
+                organization.CreatedAtUTC,
+                organization.UpdatedAtUTC);
+        }
+    }
+}
---
+++ TaskFlow.Modules/Organizations/Application/Features/GetOrganization/GetOrganizationResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskFlow.Modules.Organizations.Application.Features.GetOrganization
+{
+    public sealed record GetOrganizationResponse(
+    Guid Id,
+    string Name,
+    string Slug,
+    string Description,
+    string? LogoUrl,
+    Guid OwnerUserId,
+    bool IsActive,
+    DateTime CreatedAtUTC,
+    DateTime? UpdatedAtUTC);
+}
