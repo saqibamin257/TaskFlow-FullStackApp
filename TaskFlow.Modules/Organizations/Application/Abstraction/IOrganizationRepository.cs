d9a85e0cc50d425f2623d6d08a54303c3c1c79c5
saqibamin257 <saqibamin257@gmail.com>
2026-07-01 09:49:32 +0500

    added UpdateOrganization feature

--- TaskFlow.Modules/Organizations/Application/Abstraction/IOrganizationRepository.cs
+++ TaskFlow.Modules/Organizations/Application/Abstraction/IOrganizationRepository.cs
@@ -11,8 +11,14 @@ namespace TaskFlow.Modules.Organizations.Application.Abstraction
         Task<Organization?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
         Task<Organization?> GetBySlugAsync(string slug, CancellationToken cancellationToken = default);
         Task AddAsync(Organization organization, CancellationToken cancellationToken = default);
+        Task UpdateAsync(Organization organization, CancellationToken cancellationToken = default);
+        //create
         Task<bool> ExistsBySlugAsync(string slug, CancellationToken cancellationToken = default);
         Task<bool> ExistsByNameAsync(string name, CancellationToken cancellationToken = default);
-        Task UpdateAsync(Organization organization, CancellationToken cancellationToken = default);
+        
+        //update
+        Task<bool> ExistsBySlugExcludingOrganizationAsync(string slug,Guid organizationId, CancellationToken cancellationToken = default);
+        Task<bool> ExistsByNameExcludingOrganizationAsync(string name, Guid organizationId, CancellationToken cancellationToken = default);
+
     }
 }
