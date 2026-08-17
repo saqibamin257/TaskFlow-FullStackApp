1afc5b16504aed1726b8e7f6e977b2f796b54dfe
saqibamin257 <saqibamin257@gmail.com>
2026-08-17 09:38:01 +0500

    Added GetOrganizationByID Endpoint

---
+++ TaskFlow.Modules/Organizations/Application/Features/GetOrganization/GetOrganizationHandler.cs
@@ -0,0 +1,54 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskFlow.BuildingBlocks.Localization;
+using TaskFlow.BuildingBlocks.Security.Abstraction;
+using TaskFlow.BuildingBlocks.Security.Services;
+using TaskFlow.Modules.Organizations.Application.Abstraction;
+using TaskFlow.Modules.Organizations.Infrastructure.Repositories;
+
+namespace TaskFlow.Modules.Organizations.Application.Features.GetOrganization
+{
+    public sealed class GetOrganizationHandler: IRequestHandler<GetOrganizationQuery,GetOrganizationResponse>
+    {
+        private readonly IOrganizationRepository _organizationRepository;
+        private readonly ICurrentUser _currentUser;
+
+        public GetOrganizationHandler(IOrganizationRepository organizationRepository, ICurrentUser currentUser)
+        {
+            _organizationRepository = organizationRepository;
+            _currentUser = currentUser;
+        }
+        public async Task<GetOrganizationResponse> Handle(GetOrganizationQuery request,CancellationToken cancellationToken)
+        {
+            var organization = await _organizationRepository.GetByIdAsync(
+                request.Id,
+                cancellationToken);
+
+            if (organization is null)
+            {
+                throw new ValidationException(ErrorKeys.OrganizationNotFound);
+            }
+
+            // Only the owner can access this endpoint (for now)
+            if (organization.OwnerUserId != _currentUser.UserId)
+            {
+                throw new ValidationException(ErrorKeys.OrganizationAccessDenied);
+            }
+
+            return new GetOrganizationResponse(
+                organization.Id,
+                organization.Name,
+                organization.Slug,
+                organization.Description,
+                organization.LogoUrl,
+                organization.OwnerUserId,
+                organization.IsActive,
+                organization.CreatedAtUTC,
+                organization.UpdatedAtUTC);
+        }
+
+    }
+}
---
+++ TaskFlow.Modules/Organizations/Application/Features/GetOrganization/GetOrganizationQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskFlow.Modules.Organizations.Application.Features.GetOrganization
+{
+    public sealed record GetOrganizationQuery(Guid Id) : IRequest<GetOrganizationResponse>;
+    
+}
